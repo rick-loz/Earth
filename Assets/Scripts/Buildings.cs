41874106c385bb28a07c14259641a28a08706cc2
schoenbm <maxime.schoenberger@edu.esiee.fr>
2020-04-18 18:14:52 +0200

    Factory Working

--- Assets/Scripts/Buildings.cs
+++ Assets/Scripts/Buildings.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class Buildings : MonoBehaviour
 {
+    private Ressources ressources;
     private Slices parentSlice;
-    private int buildingValue;
-    private int upgradeValue;
-    private int sellValue;
+
+    private int lvl;
+    private bool onCd = true;
+
+    public int buildingValue;
+    public int[] upgradesValues;
+    public int[] cd;
+    public int sellValue;
     
     public int getBuildingValue() { return buildingValue; }
 
     public int getSellValue() { return sellValue; }
     
-    public void upgrade() { }
+    public void Upgrade() { }
 
-    public void execute() { }
+    public void Active() { }
+
+    public void Sell() { }
+
+    public Ressources getRessources() { return this.ressources; }
+
+    public void Built() {}
+
+    public int getLvl() { return lvl; }
+
+    public void addLvl() { this.lvl++; }
+
+    public bool getOnCd() { return this.onCd; }
+
+    public IEnumerator startCd()
+    {
+        this.onCd = false;
+        float elapsed = 0.0f;
+        while (elapsed < this.cd[this.lvl])
+        {
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        this.onCd = true;
+    }
 }
--- Assets/Scripts/Buildings/Factory.cs
+++ Assets/Scripts/Buildings/Factory.cs
@@ -2,17 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Factory : MonoBehaviour
+public class Factory : Buildings
 {
-    // Start is called before the first frame update
-    void Start()
+    public int[] moneyIncome;
+    public int[] wasteIncome;
+
+    private bool isOn;
+    new public void Built()
     {
-        
+        this.isOn = true;
+        this.getRessources().addIncomeMoney(this.moneyIncome[0]);
+        this.getRessources().addIncomeWaste(this.wasteIncome[0]);
     }
 
-    // Update is called once per frame
-    void Update()
+    new public void Upgrade()
     {
-        
+        this.getRessources().looseIncomeMoney(this.moneyIncome[this.getLvl()]);
+        this.getRessources().looseIncomeWaste(this.wasteIncome[this.getLvl()]);
+
+        this.addLvl();
+
+        this.getRessources().addIncomeMoney(this.getLvl());
+        this.getRessources().addIncomeWaste(this.getLvl());
+    }
+
+    new public void Sell()
+    {
+        this.getRessources().looseIncomeMoney(this.moneyIncome[this.getLvl()]);
+        this.getRessources().looseIncomeWaste(this.wasteIncome[this.getLvl()]);
+
+        Destroy(this);
+    }
+
+    new public void Active()
+    {
+        if(this.getOnCd() && !this.isOn)
+        {
+            StartCoroutine(this.startCd());
+            this.getRessources().addIncomeMoney(this.getLvl());
+            this.getRessources().addIncomeWaste(this.getLvl());
+            this.isOn = true;
+        }
+        else if(this.getOnCd() && this.isOn)
+        {
+            StartCoroutine(this.startCd());
+            this.getRessources().looseIncomeMoney(this.moneyIncome[this.getLvl()]);
+            this.getRessources().looseIncomeWaste(this.wasteIncome[this.getLvl()]);
+            this.isOn = false;
+        }
     }
 }
